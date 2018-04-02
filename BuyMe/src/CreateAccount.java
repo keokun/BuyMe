3482a28980b423457d97c9cc3bd37ccd9a890271
JoshPineda <jpinedainbox@gmail.com>
2018-04-02 11:23:20 -0400

    changed accountType to type

--- BuyMe/src/CreateAccount.java
+++ BuyMe/src/CreateAccount.java
@@ -50,7 +50,7 @@ public class CreateAccount extends HttpServlet {
 			      conn = DriverManager.getConnection(BuyMe.DB_URL,BuyMe.USER,BuyMe.PASS);
 
 			      String sql;
-			      sql = "INSERT INTO Account (accountType, username, pass, email, fname, lname)"
+			      sql = "INSERT INTO Account (type, username, pass, email, fname, lname)"
 			      		+ "VALUES ('regular', '" + username + "', '" + password + "', '" + email + "', '" + fname + "', '" + lname + "')";
 			      stmt=conn.prepareStatement(sql);
 			      stmt.executeUpdate();
