02e8c48d703e89ee1f5e61b65d58999f87156da9
JoshPineda <jpinedainbox@gmail.com>
2018-04-16 16:40:27 -0400

    Fixed issue with NullPointerException on account with no type

--- BuyMe/src/Home.java
+++ BuyMe/src/Home.java
@@ -16,20 +16,35 @@ public class Home extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String type=(String) request.getSession().getAttribute("type");
 		String username=(String)request.getSession().getAttribute("username");
-		
-		if(type.equals("regular")) {
-			response.sendRedirect(request.getContextPath()+"/AuctionHistory?user=" + username);
+		if (type != null)
+		{
+			if(type.equals("regular")) {
+				response.sendRedirect(request.getContextPath()+"/AuctionHistory?user=" + username);
+			}
+			
+			else if (type.equals("rep")) {
+				RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/jsps/rephome.jsp");
+		    	dispatcher.forward(request,response);
+			}
+			
+			else if (type.equals("admin")){
+				RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/jsps/adminpage.jsp");
+		    	dispatcher.forward(request,response);
+			}
+			
+			else
+			{
+				RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/jsps/login.jsp");
+				dispatcher.forward(request, response);
+			}
+		}
+		//if the type is null, then we just have to go back to the login 
+		else
+		{
+			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/jsps/login.jsp");
+			dispatcher.forward(request, response);
 		}
 		
-		else if (type.equals("rep")) {
-			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/jsps/rephome.jsp");
-	    	dispatcher.forward(request,response);
-		}
-		
-		else {
-			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/jsps/adminpage.jsp");
-	    	dispatcher.forward(request,response);
-		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
